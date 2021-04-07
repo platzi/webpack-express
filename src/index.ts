e1637f2a6dda0decb662f43502a184d758296cec
Oscar Barajas <oscar@dragons.mx>
2021-04-07 12:30:21 -0500

    :art: [ADD] Clase 05

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 import { Request, Response } from 'express';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const initialState = {
   cart: [],
